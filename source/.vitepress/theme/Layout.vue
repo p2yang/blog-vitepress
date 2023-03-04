7003fff5f219e8b652796cd83a0339ac140a6815
pyang <p2yang@163.com>
2023-03-04 17:51:13 +0800

    add custom layout with content title & post time

---
+++ source/.vitepress/theme/Layout.vue
@@ -0,0 +1,32 @@
+<!--.vitepress/theme/MyLayout.vue-->
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+const formateDate = date => new Date(date).toDateString()
+
+</script>
+
+<template>
+  <Layout>
+    <template #doc-before>
+      <h1 class="content-title">{{ $frontmatter.title }}</h1>
+
+      <p class="content-meta">
+      	<span>{{ formateDate($frontmatter.date) }}</span>
+      </p>
+    </template>
+  </Layout>
+</template>
+
+<style scoped>
+.content-title {
+	font-size: 24px;
+}
+
+.content-meta {
+	margin: 5px 0;
+	font-size: 12px;
+	color: #999;
+}
+</style>
