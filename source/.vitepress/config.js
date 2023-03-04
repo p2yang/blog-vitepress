09e1ea3da47d96cc5019ff8ff8ade3f7b850a83d
pyang <p2yang@163.com>
2023-03-04 16:53:49 +0800

    add navs

--- source/.vitepress/config.js
+++ source/.vitepress/config.js
@@ -1,5 +1,13 @@
 export default {
   title: 'Joe\'s blog',
   description: '技术与生活纪事，涵盖react、vue、node、docker、elasticsearch、linux等。',
-  lastUpdated: true
+  lastUpdated: true,
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Archives', link: '/archives/' },
+      { text: 'Tags', link: '/tags/' },
+      { text: 'Categories', link: '/categories/' }
+    ]
+  }
 }
